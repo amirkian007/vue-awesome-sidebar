ae4df162943116eedfc5bbdd4b369cd4f1416a0f
amirkian007 <68250433+amirkian007@users.noreply.github.com>
2022-08-19 22:02:14 +0430

    Add router hook

--- src/hooks/useAwsomeRouter.ts
+++ src/hooks/useAwsomeRouter.ts
@@ -1,67 +1,37 @@
 import { ref, computed, inject, provide, reactive, toRefs } from 'vue'
 
-export const initAwsomeSideBar = (props: any, context: any) => {
+export const initAwsomeRouter = (props: any, context: any) => {
   const { autoCollapse, collapsed, relative, width, widthCollapsed, rtl } =
     toRefs(props)
-  const isCollapsed = ref(collapsed.value)
-  const slots = ref(context.slots)
-  const currentRoute = ref(
-    window.location.pathname + window.location.search + window.location.hash
-  )
+  const currentRoute = ref(window.location)
+  window.addEventListener('hashchange', function () {
+    currentRoute.value = window.location
+    //console.log( window.location.pathname + window.location.search + window.location.hash)
+  })
 
-  function isSameUrl(url, location = currentRoute) {
-    location = location || document.location
-    var match = url.match(
-      /^([^:\/?#]+:)?(?:\/\/([^\/?#]+))?([^?#]+)?(\?[^#]*)?(#.*)?/
+  function isSameUrl(url, location = currentRoute.value) {
+    var path = function (s) {
+      return s.replace(/\?.*$/, '').replace(/\#.*$/, '')
+    }
+    return (
+      path(location.href) == url ||
+      path(location.pathname + location.hash) == url ||
+      location.hash == url
     )
-    if (
-      typeof match[1] === 'string' &&
-      match[1].length > 0 &&
-      match[1].toLowerCase() !== location.protocol
-    )
-      return false
-    if (
-      typeof match[2] === 'string' &&
-      match[2].length > 0 &&
-      match[2].replace(
-        new RegExp(
-          ':(' + { 'http:': 80, 'https:': 443 }[location.protocol] + ')?$'
-        ),
-        ''
-      ) !== location.host
-    )
-      return false
-    if (
-      typeof match[3] === 'string' &&
-      match[3].length > 0 &&
-      match[3] !== location.pathname
-    )
-      return false
-    if (
-      typeof match[4] === 'string' &&
-      match[4].length > 0 &&
-      match[4] !== location.search
-    )
-      return false
-    if (
-      typeof match[5] === 'string' &&
-      match[5].length > 0 &&
-      match[5] !== location.hash
-    )
-      return false
-    return true
   }
   function extractChildrenRoutes(obj, keyToFind) {
+    if (!obj) return
     return Object.entries(obj).reduce(
       (acc, [key, value]) =>
         key === keyToFind
           ? acc.concat(value)
           : typeof value === 'object'
-          ? acc.concat(findAllByKey(value, keyToFind))
+          ? acc.concat(extractChildrenRoutes(value, keyToFind))
           : acc,
       []
     )
   }
+
   //  if(autoCollapse.value){
   //  }
 
@@ -72,24 +42,12 @@ export const initAwsomeSideBar = (props: any, context: any) => {
     slots.value = val
   }
 
-  const updateCurrentRoute = () => {
-    currentRoute.value =
-      window.location.pathname + window.location.search + window.location.hash
-  }
-
-  //   const onItemClick = (event, item) => {
-  //     context.emit('item-click', event, item)
-  //   }
-  let userAgentHeight =
-    navigator.userAgent.indexOf('Firefox') != -1
-      ? '-moz-max-content'
-      : 'fit-content'
-
-  provide('sidebarProps', props)
-  provide('getSlotByName', getSlotByName)
-  provide('browserAgent', userAgentHeight)
+  provide('currentRoute', currentRoute)
+  provide('isSameUrl', isSameUrl)
+  provide('extractChildrenRoutes', extractChildrenRoutes)
   return {
     isSameUrl,
-    extractChildrenRoutes
+    extractChildrenRoutes,
+    currentRoute
   }
 }
