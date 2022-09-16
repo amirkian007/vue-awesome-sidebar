c1b1ea1cd62b4da046eb9fade8c9cfa5d82c05bc
amirkian007 <68250433+amirkian007@users.noreply.github.com>
2022-09-16 14:37:15 +0430

    FORMAT some code

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
 import 'material-icons/iconfont/material-icons.css'
 import { createRouter, createWebHistory } from 'vue-router'
@@ -18,16 +17,19 @@ const routes = [
   { path: '/a/b', component: ComC },
   { path: '/a/b/c', component: ComA },
   { path: '/a/b/c/d', component: CompB },
+  { path: '/k', component: ComC }
 ]
 
 // 3. Create the router instance and pass the `routes` option
 // You can pass in additional options here, but let's
 // keep it simple for now.
-console.log(import.meta.url)
 const router = createRouter({
   // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
   strict: true,
   history: createWebHistory(''),
-  routes, // short for `routes: routes`
+  routes // short for `routes: routes`
 })
+
+console.log(App)
+console.log(App.setup)
 createApp(App).use(router).mount('#app')
