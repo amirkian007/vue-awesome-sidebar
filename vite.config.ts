23cee847804d3ad3d19163959fa71669b8de998d
amirkian007 <68250433+amirkian007@users.noreply.github.com>
2022-09-24 00:23:33 +0330

    Add ne build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,25 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import { terser } from 'rollup-plugin-terser'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-  // build: {
-  //   // cssCodeSplit: false,
-  //   // lib: {
-  //   //   entry: "./src/VueAwesomeSiderbar.ts",
-  //   //   formats: ["es", "cjs"],
-  //   //   name: "VueAwesomeSiderbar",
-  //   //   fileName: (format) => (format === "es" ? "index.js" : "index.cjs"),
-  //   // },
-  //   // rollupOptions: {
-  //   //   external: ["vue"],
-  //   //   output: {
-  //   //     globals: {
-  //   //       vue: "Vue",
-  //   //     },
-  //   //   },
-  //   // },
-  // },
+  plugins: [vue()],
+  server: {},
+  build: {
+    lib: {
+      entry: './src/VueAwesomeSiderbar.ts',
+      formats: ['es', 'cjs'],
+      name: 'VueAwesomeSiderbar',
+      fileName: (format) => (format === 'es' ? 'index.js' : 'index.cjs')
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue'
+        }
+      },
+      plugins: [terser()]
+    }
+  }
 })
