85e4a6970d06bc224f5d24bde356cce8cd6d0dd6
amirkian007 <68250433+amirkian007@users.noreply.github.com>
2022-11-02 01:53:19 +0330

    Add some test routes

--- src/main.ts
+++ src/main.ts
@@ -4,20 +4,22 @@ import 'material-icons/iconfont/material-icons.css'
 import { createRouter, createWebHistory } from 'vue-router'
 
 import MenuItemIcon from './components/MenuItemIcon.vue'
-import ComA from './ComA.vue'
-import CompB from './CompB.vue'
-import ComC from './ComC.vue'
-
+const Home = { template: '<div>Home</div>' }
+const About = { template: '<div>About</div>' }
+const test = { template: '<div>test</div>' }
+const test2 = { template: '<div>test2</div>' }
+const test3 = { template: '<div>test3</div>' }
+const test4 = { template: '<div>test4</div>' }
 // 2. Define some routes
 // Each route should map to a component.
 // We'll talk about nested routes later.
 const routes = [
-  { path: '/', component: ComA },
-  { path: '/a', component: CompB },
-  { path: '/a/b', component: ComC },
-  { path: '/a/b/c', component: ComA },
-  { path: '/a/b/c/d', component: CompB },
-  { path: '/k', component: ComC }
+  { path: '/', component: Home },
+  { path: '/a', component: About },
+  { path: '/a/b', component: test },
+  { path: '/a/b/c', component: test2 },
+  { path: '/a/b/c/d', component: test3 },
+  { path: '/k', component: test4 }
 ]
 
 // 3. Create the router instance and pass the `routes` option
